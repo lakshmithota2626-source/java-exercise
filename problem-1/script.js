d9dec94f6cfb87531ea6fed4fd4cbf5af08d5aa9
lakshmithota2626-source <lakshmithota2626@gmail.com>
2026-09-24 21:19:55 +0530

    Add initial JavaScript file with console and alert messages

---
+++ problem-1/script.js
@@ -0,0 +1,6 @@
+console.log("JavaScript file linked successfully!");
+
+document.getElementById("msg").innerText =
+    "Hello! This message comes from script.js";
+
+alert("Welcome! script.js is connected to index.html");
